cbca2ac043fc33c3c9d6a5a41f7cf27c50c475e5
Abdülkadir Çakır <afcsoft@outlook.com>
2019-11-22 05:39:49 +0300

    initial commit2

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const pool=new pg({host:'ec2-54-225-195-3.compute-1.amazonaws.com',
                        password:'f0d3555952350a1a428baf233e4d10157a6f8e11e7ad58059c832cff5626bbdc',
                        port:'5432',ssl:'true'});
 
+var data;
 
-pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
+
+
+
 
 const _port = process.env.PORT || 5000;
 const _app_folder = 'dist/';
@@ -24,6 +24,16 @@ app.use(compression());
 app.use(express.static(__dirname + '/dist' ));
 // ---- SERVE STATIC FILES ---- //
 app.post('*.*', express.static(_app_folder, {maxAge: '1y'}));
+
+app.get("/api/test",function(req,res)
+{
+        pool.query("SELECT row_to_json(fc) FROM ( SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features FROM (SELECT 'Feature' As type, ST_AsGeoJSON(lg.geom)::json As geometry, row_to_json((id,name)) As properties FROM trees_test As lg) As f) As fc", (err, res1) => 
+        {
+        res.send(res1.rows[0].row_to_json)
+        pool.end()
+        })
+}
+);
 
 // ---- SERVE APPLICATION PATHS ---- //
 app.all('*', function (req, res) {
